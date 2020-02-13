cf41103bba2745c0c195231e45b3f685ebfc0dfb
jmservera <jmservera@outlook.com>
2020-02-13 19:15:21 +0100

    remove unused code

--- tests/common.py
+++ tests/common.py
@@ -2,7 +2,3 @@ import os
 
 def get_fixture_path(filename):
     return os.path.join(os.path.dirname(__file__), "fixtures", filename)
-
-def run_func(func, params, mock_out)->str:
-    func(**params)
-    return mock_out.getvalue()
